6f9bd79677f0361479bce541ee735b966afaf1a3
Rohan Jetha <rohanjetha2@gmail.com>
2026-02-03 17:02:06 +1100

    hotjar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,19 @@ import "./globals.css";
 import SiteFooter from "@/components/SiteFooter";
 import { SiteNav } from "@/components/site/SiteNav";
 import "@/features/ai-money-coach/ai-money-coach.css";
+import Script from "next/script";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          id="contentsquare-tag"
+          strategy="afterInteractive"
+          src="https://t.contentsquare.net/uxa/4db637a313d77.js"
+        />
+      </head>
+
       <body>
         <SiteNav />
         {children}
